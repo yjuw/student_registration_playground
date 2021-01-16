27a57cc4558f0d4d5aed4f1f79750e1598d406b2
yjyj <byk299@uw.edu>
2021-01-16 14:45:10 -0800

    first commit, created sql tables

---
+++ planning/create_tables.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS Course(
+course_id SERIAL PRIMARY KEY,
+course_name TEXT,
+course_name_short TEXT,
+course_capacity INTEGER,
+course_currently_enrolled INTEGER,
+course_instructor TEXT,
+course_section_number INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS Member(
+net_id TEXT PRIMARY KEY,
+first_name TEXT,
+middle_name TEXT,
+last_name TEXT,
+hashed_pass TEXT,
+role TEXT
+);
+
+CREATE TABLE IF NOT EXISTS Enrolled_Class(
+Enrolled_Class_id SERIAL PRIMARY KEY,
+Enrolled_Class_Member_id TEXT REFERENCES Member(net_id),
+Enrolled_Course_id INTEGER REFERENCES Course(course_id)
+);
