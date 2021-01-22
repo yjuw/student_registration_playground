a7b1995f4625dbd6325bc4fba4d7cfae04fc4ba3
yjyj <byk299@uw.edu>
2021-01-21 21:29:57 -0800

    fixed apache again :/ Need to fix import bcrypt

--- cgi-bin/my_cgi.py
+++ cgi-bin/my_cgi.py
@@ -1,20 +1,48 @@
 #!/usr/bin/env python3
 import cgi
-
+import psycopg2
+import json
 import cgitb
+import bcrypt
 cgitb.enable(display=0, logdir="/test/")
 
 form = cgi.FieldStorage()
-first_name = form.getvalue("uname")
-password = form.getvalue("psw")
+net_id = form.getvalue("uname")
+upass = form.getvalue("psw")
 
-print("Content-type:text/html")
-print("")
-print("")
-print("Hello")
-print("")
-print("")
-print("")
-print(" Hello", first_name)
-print("")
-print("")
+with open("/var/www/planning/postgre_credentials.json") as file:
+    data = json.load(file)
+with open("/var/www/planning/salt.txt", "r") as file:
+      salt = file.read()
+
+connection = psycopg2.connect(
+database = data["db_name"], 
+user = data["db_user"],
+password = data["db_password"],
+host = data["db_host"],
+port = data["db_port"])
+cur = connection.cursor()
+
+#upass_salted = bcrypt.hashpw(bytes(upass), bytes(salt))
+
+try:
+       # member = cur.execute("""SELECT net_id
+       #                      FROM member
+       #                      WHERE net_id = %s
+       #                      AND hashed_pass = %s""",
+       #                      (net_id, upass_salted))
+       print ("Content-type:text/html\n") 
+       print ("<html>") 
+       print ("<head>") 
+       print ("<title>First CGI Program</title>") 
+       print ("</head>") 
+       print ("<body>") 
+       print("testerino")
+       print ("</body>") 
+       print ("</html>")
+except:
+       print("error")
+
+
+finally:
+       connection.close()
